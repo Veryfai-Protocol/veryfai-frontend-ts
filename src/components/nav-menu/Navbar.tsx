808e20b87a4c3bee76873b75018db60dbe14787c
Favour Sunday <sundayfavour997@gmail.com>
2024-10-01 16:31:58 +0100

    quick navbar fix

--- src/components/nav-menu/Navbar.tsx
+++ src/components/nav-menu/Navbar.tsx
@@ -23,10 +23,11 @@ export const Navbar = ({input} : InputProp) => {
         <div>
           <Logo />
         </div>
-        <div className="flex justify-start relative">
+        <div className="w-full">
+        <div className="flex justify-start relative w-full md:w-[70%]">
           <Input
             type="text"
-            className={`rounded-full bg-[#F3F4F6] py-5 pl-6 pr-16 w-[400px] md:w-[500px] lg:w-[700px] text-gray-700 focus:outline-none`}
+            className={`rounded-full bg-[#F3F4F6] py-5 pl-6 pr-16 w-full md:w-[500px] lg:w-[700px] text-gray-700 focus:outline-none`}
             placeholder="Type your statement here"
             value={input}
             onChange={handleInputChange}
@@ -34,6 +35,7 @@ export const Navbar = ({input} : InputProp) => {
           <Button className="bg-[#1E90FF] absolute right-2 top-1/2 transform -translate-y-1/2 text-white rounded-full flex items-center justify-center hover:bg-blue-600">
             <CiSearch className="w-5 h-5" />
           </Button>
+        </div>
         </div>
       </nav>
     </header>
